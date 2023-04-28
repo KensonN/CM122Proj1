94cf2a83e28483e7e585b6f99c0063b04ac44433
KensonN <kensonn@ucla.edu>
2023-04-27 21:19:06 -0700

    submission code; step size not implemented properly yet so keep at 1

--- Project1A.py
+++ Project1A.py
@@ -20,6 +20,7 @@ class readAligner():
     finalDeletions = dict()
     finalSubstitutions = dict()
     positions = []
+    stepSize = 1
 
     def __init__(self, genomeFile, readFile):
         self.genomeFile = genomeFile
@@ -128,72 +129,119 @@ class readAligner():
                 mutation.append((i+alignment, readString[i], genomeString[i]))
         return mutation
 
-    # def getStreak(self, positions):
-    #     streakArray = []
-    #     for count, pos in enumerate(positions):
-    #         if pos != [-10]:
-    #             streakArray.append(pos)
-    #     streaks = []
-    #     # print(streakArray)
-    #     # sum = 0
-    #     # for i in streakArray:
-    #     #     sum+=i[0]
-    #     # sum /= len(streakArray)
-    #     # print("avg", sum)
-    #     for count, streakPos in enumerate(streakArray):
-    #         concurrentNumbers = [streakPos[0]]
-
-    #         cont = False
-    #         for o in streaks:
-    #             print(streakPos[0], "pos")
-    #             print(o, "o")
-    #             if streakPos[0] in o:
-    #                 cont = True
-    #                 break
-
-    #         if cont == True:
-    #             continue
-
-    #         for k in range(count, len(streakArray)-1):
-    #             print(streakArray[k])
-    #             cont = False
-    #             for o in streaks:
-    #                 # print(streakArray[k][0], o)
-    #                 pass
-
-    #             if streakArray[k+1][0] == (streakArray[k][0] + 1):
-    #                 concurrentNumbers.append(streakArray[k+1][0])
-    #                 # print(streakArray[k+1][0], "concurrent")
-    #             else:
-    #                 continue
-    #                 # last+=1
-    #         streaks.append(concurrentNumbers)
-    #     print(streaks, "concurrent")
-    #         pass
-        
-        # print(streakArray)
-        # streak = [-10] * len(positions)
+    def getStreak(self, positions):
+        # streakArray = []
         # for count, pos in enumerate(positions):
-        #     streakArray = []
-        #     if pos == [-10]:
-        #         continue
-        #     for j in pos:
-        #         # streakArray.append(j)
-        #         last = j
-        #         print(j)
-        #         print("---")
-        #         for p in streakArray:
-        #             if j in p:
-        #                 continue
-        #         for k in range(count, len(positions)):
-        #             if last+1 in positions[k]:
-        #                 print(last+1)
-        #                 last+=1
-        #         pass
-        #     # streak[count-].append(streakArray)
-        #     print()
-        # print(streak, "streak")
+        #     if pos != [-10]:
+        #         streakArray.append(pos)
+        # streaks = []
+        avg = 0
+        counter = 0
+        for i in positions:
+            if i != [-10] and len(i) == 1:
+                avg += i[0]
+                counter+=1
+        avg /= counter
+        # print(positions)
+        # print("avg", avg)
+        positionsReduced = []
+        for i in positions:
+            if i!= [-10] and len(i) > 1:
+                index = 0
+                diff = 999999
+                for count, arrayItem in enumerate(i):
+                    if abs(arrayItem - avg) < diff:
+                        diff = abs(arrayItem - avg)
+                        index = count
+                positionsReduced.append(i[index])
+            else:
+                positionsReduced.append(i[0])
+        # print(positionsReduced, "posRedux")
+
+        streak = False
+        currentStreakSum= 0
+        currentStreakStartIndex = 0
+        currentStreakCount = 0
+        streakAvgs = []
+        streakIndexes = []
+        streakCounts = []
+        last = None
+        for count, i in enumerate(positionsReduced):
+            # print("count:", count)
+            # print(last)
+            if i != -10:
+                if streak == False:
+                    # print(i)
+                    streak = True
+                    currentStreakStartIndex = count
+                    currentStreakSum = i
+                    currentStreakCount = 1
+                elif streak == True:
+                    if abs(i - last)/self.stepSize < 2:
+                        currentStreakSum += i
+                        currentStreakCount += 1
+                        if count == (len(positionsReduced)-1):
+                            streak = False
+                            streakAvgs.append(currentStreakSum/currentStreakCount)
+                            streakIndexes.append(currentStreakStartIndex)
+                            streakCounts.append(currentStreakCount)
+                    else:
+                        # print("else")
+                        streakAvgs.append(currentStreakSum/currentStreakCount)
+                        streakIndexes.append(currentStreakStartIndex)
+                        streakCounts.append(currentStreakCount)
+                        currentStreakStartIndex = count
+                        currentStreakSum = i
+                        currentStreakCount = 1
+                last = i
+            elif i == -10:
+                if streak == True:
+                    streak = False
+                    streakAvgs.append(currentStreakSum/currentStreakCount)
+                    streakIndexes.append(currentStreakStartIndex)
+                    streakCounts.append(currentStreakCount)
+            # return positionsReduced
+        # print(streakAvgs)
+        # print(streakIndexes)
+        # print(streakCounts)
+        # print(positionsReduced)
+
+        maxStreakCountIndex = streakCounts.index(max(streakCounts))
+        maxStreakStartIndex = streakIndexes[maxStreakCountIndex]
+
+        streakIndexes.pop(maxStreakCountIndex)
+        streakCounts.pop(maxStreakCountIndex)
+
+        # print(streakAvgs)
+        # print(streakIndexes)
+        # print(streakCounts)
+        # print(positionsReduced)
+
+        # print(streakAvgs[streakCounts.index(max(streakCounts))])
+        
+        # print("---")
+        for count, i in enumerate(streakIndexes):
+            # print(i, i+streakCounts[count])
+            for j in range(i, i+streakCounts[count]):
+                # print(j, end=" ")
+                # print()
+                # print(abs(maxStreakStartIndex - j))
+                # print(abs(positionsReduced[maxStreakStartIndex] - positionsReduced[j]))
+                if abs(abs(maxStreakStartIndex - j) - abs(positionsReduced[maxStreakStartIndex] - positionsReduced[j])) > 3:
+                    # print("pop:", j)
+                    positionsReduced[j] = -10
+                pass
+                # print("--")
+            # print("---")
+            pass        
         pass
+        # print('\n\n\n')
+        return positionsReduced
+        returnArray = []
+        for i in positionsReduced:
+            returnArray.append([i])
+        # print(returnArray)
+        return returnArray
 
     def align(self):
         # firstPair = True
@@ -204,6 +252,7 @@ class readAligner():
             # print(lastReadPos)
             # print(read)
             readSplit = read[0].split("/")
+            # lastReadPos = 0
             if len(readSplit) > 1:
                 if read[0].split("/")[1] == '1':
                     lastReadPos = 0
@@ -211,14 +260,16 @@ class readAligner():
                 lastReadPos = 0
 
             currentTime = time.perf_counter() - startTime
+            # print(read[0], '\n', "readPos:", lastReadPos, '\n', "currentTime:", round(currentTime, 3), '\n', "readTime:",  round(currentTime - lastCurrentTime, 3))
+
             if count == len(self.reads) - 1:
                 print(read[0], lastReadPos, round(currentTime, 3), round(currentTime - lastCurrentTime, 3), end='\n')
             else:
-                print(read[0], lastReadPos, round(currentTime, 3), round(currentTime - lastCurrentTime, 3), end='\r')
+                print(read[0], lastReadPos, round(currentTime, 3), round(currentTime - lastCurrentTime, 3), end='\n')
             lastCurrentTime = currentTime
             positions = [[-10]] * (len(read[1])-self.kmerSize+1)
             # print(i[0], i[1], len(i[1])-self.kmerSize+1)
-            for kmerPos in range(0, len(read[1])-self.kmerSize+1):
+            for kmerPos in range(0, len(read[1])-self.kmerSize+1, self.stepSize):
                 keys = list(self.genomeMap.keys())[lastReadPos:]
                 for key in keys:
                     # print(j)
@@ -229,6 +280,12 @@ class readAligner():
                             positions[kmerPos] += deepcopy(self.genomeMap[key])
                 # total += 1
             # print(positions, "pos")
+
+            if len(positions) == 0:
+                continue
+
+            positionsFiltered = self.getStreak(positions)
+            
             finalPositions = []
             # for i in range(len(positions)):
             #     if positions[i] == [-10]:
@@ -238,64 +295,63 @@ class readAligner():
             counter = 0
             streak = 0
             # print(positions)
-            for pos in range(len(positions)):
-                for count, posItem in enumerate(positions[pos]):
+            for pos, posItem in enumerate(positionsFiltered):
+            # for pos in range(len(positions)):
+            #     for count, posItem in enumerate(positions[pos]):
                     # print("posItem=", posItem, "counter:", counter, "streak:", streak, "last:", last)
-                    if last == None:
-                        if posItem == -10:
-                            finalPositions.append(posItem)
-                            counter += 1
-                        else:
+                if last == None:
+                    if posItem == -10:
+                        finalPositions.append(posItem)
+                        counter += 1
+                    else:
+                        last = posItem
+                        finalPositions.append(posItem)
+                        streak+=1
+                        if pos > 0:
+                            for l in range(1, counter+1):
+                                # print(pos-l-1, "posl")
+                                finalPositions[pos-l] = posItem - l
+                                streak+=1
+                                pass
+                elif posItem != -10:
+                    if abs((posItem-last))/self.stepSize <= 5:
+                        # print("if")
+                        last = posItem
+                        finalPositions.append(posItem)
+                        streak += 1
+                        counter = 0
+                    else:
+                        if streak < 2:
+                            for l in range(0, counter+1):
+                                # print(pos-l-1, "posl")
+                                finalPositions[pos-l-1] = -10
+                                pass
                             last = posItem
                             finalPositions.append(posItem)
-                            streak+=1
-                            if pos > 0:
-                                for l in range(1, counter+1):
-                                    # print(pos-l-1, "posl")
-                                    finalPositions[pos-l] = posItem - l
-                                    streak+=1
-                                    pass
-                    elif posItem != -10:
-                        if abs((posItem-last)) <= 5:
-                            # print("if")
-                            last = posItem
-                            finalPositions.append(posItem)
-                            streak += 1
                             counter = 0
-                        else:
-                            if streak < 2:
-                                for l in range(0, counter+1):
-                                    # print(pos-l-1, "posl")
-                                    finalPositions[pos-l-1] = -10
-                                    pass
-                                last = posItem
-                                finalPositions.append(posItem)
-                                counter = 0
-                            finalPositions.append(last+1)
-                            last +=1
-                    elif posItem == -10:
-                        # print("-10")
-                        # if counter <= 2*self.kmerSize:
-                        last += 1
-                        finalPositions.append(last)
-                        counter+=1
+                        finalPositions.append(last+1*self.stepSize)
+                        last +=1
+                elif posItem == -10:
+                    # print("-10")
+                    # if counter <= 2*self.kmerSize:
+                    last += 1*self.stepSize
+                    finalPositions.append(last)
+                    counter+=1
             last = None
             counter = 0
 
-            if len(finalPositions) == 0:
-                continue
-            
             #fill in -10s            
             if finalPositions[0] == -10:
                 for count, item in enumerate(finalPositions):
                     if item != -10:
                         for i in range(1, count+1):
-                            finalPositions[count - i] = item - i
+                            finalPositions[count - i] = item - i*self.stepSize
                         break
 
             first = None
             finalPositionsTwo = deepcopy(finalPositions)
             modifiedRead = read[1]
+
             # print(modifiedRead, "read")
             # print(self.genome[finalPositions[0]:finalPositions[0]+50], "genome")
 
@@ -320,6 +376,7 @@ class readAligner():
                     continue
                 if k != first + 1:
                     diff = k - first
+                    diff /= self.stepSize
                     if diff == 2:
                         deletionLoc = int((k+first)/2)
                         self.coverageMap[deletionLoc] += 1
@@ -360,6 +417,21 @@ class readAligner():
                 first = k
             self.positions.append(finalPositionsTwo)
 
+            print(positions, "positions")
+            print(positionsFiltered, "positionsFiltered")
+            print(finalPositions, "finalPos")
+            print(finalPositionsTwo, "finalPosTwo")
+            print("---")
+
+            print(len(self.genome) - self.kmerSize)
+            for i in finalPositionsTwo:
+                if i > len(self.genome) - self.kmerSize:
+                    finalPositionsTwo.remove(i)
+            # if read[0] == ">read_3/2":
+            #     break
+            
+            print(finalPositionsTwo, "finalPosTwo")
+            print("---")
 
             lastPos = None
             if len(finalPositionsTwo) > 0:
@@ -369,17 +441,12 @@ class readAligner():
                         lastPos = pos
                 if lastPos != None:
                     for j in range(1, self.kmerSize):
+                        print(lastPos+j)
                         self.coverageMap[lastPos+j] += 1
-                    lastReadPos = lastPos+self.kmerSize-1
+
+                lastReadPos = finalPositionsTwo[0]
                 stringAlignment = 0
                 genomeString = ""
-
-                # self.getStreak(positions)
-
-                # print(positions, "positions")
-                # print(finalPositions, "finalPos")
-                # print(finalPositionsTwo, "finalPosTwo")
-                # print("---")
 
                 if finalPositionsTwo[0] == -10 and finalPositionsTwo[-1] == -10:
                     stringAlignment = 999
@@ -427,19 +494,21 @@ class readAligner():
 
         print("Deletions:" )
         for i in self.deletions:
-            if self.deletions[i] / self.coverageMap[i[0]] > 0.20 and self.coverageMap[i[0]] > 1 and self.deletions[i] > 1:
+            # if self.deletions[i] / self.coverageMap[i[0]] > 0.20 and self.coverageMap[i[0]] > 1 and self.deletions[i] > 1:
+            if self.coverageMap[i[0]] > 1 and self.deletions[i] > 1:
                 self.finalDeletions[i] = self.deletions[i]
                 print(i, self.deletions[i], self.coverageMap[i[0]])
         # print("---")
         # print("deletions: ", self.deletions)
         print("Insertions: ")
         for i in self.insertions:
-            if self.insertions[i] / self.coverageMap[i[0]] > 0.20 and self.coverageMap[i[0]] > 1 and self.insertions[i] > 1:
+            if self.coverageMap[i[0]] > 1 and self.insertions[i] > 1:
+            # if self.insertions[i] / self.coverageMap[i[0]] > 0.20 and self.coverageMap[i[0]] > 1 and self.insertions[i] > 1:
                 self.finalInsertions[i] = self.insertions[i]
                 print(i, self.insertions[i], self.coverageMap[i[0]])
         # print("---")
         # print("Substitutions:")
-        print("Subsitutions")
+        print("Substitutions")
         for i in self.substitutions:
             if self.substitutions[i] / self.coverageMap[i[0]] > 0.5 and self.coverageMap[i[0]] > 1 and self.substitutions[i] > 1:
                 self.finalSubstitutions[i] = self.substitutions[i]
@@ -470,22 +539,22 @@ class readAligner():
 
 
 
-# referenceGenome = "sample_1000\sample_1000_reference_genome.fasta"
+referenceGenome = "sample_1000\sample_1000_reference_genome.fasta"
 # referenceGenome = "project1a_10000_reference_genome.fasta"
 
 # reads = "sample_1000\sample_1000_no_error_single_reads.fasta"
 # reads = "sample_1000\sample_1000_with_error_single_reads.fasta"
 # reads = "sample_1000\sample_1000_with_error_paired_reads.fasta"
 # reads = "sample_1000\sample_1000_no_error_paired_reads REDUCED.fasta"
-# reads = "sample_1000\sample_1000_no_error_paired_reads.fasta"
+reads = "sample_1000\sample_1000_no_error_paired_reads.fasta"
 # reads = "sample_1000\TEST.fasta"
 # reads = "sample_1000\DELETION.fasta"
 # reads = "sample_1000\INSERTION.fasta"
 # reads = "sample_1000\REDUCED.fasta"
 # reads = "project1a_10000_with_error_paired_reads.fasta"
-# reads = "project1a_10000_with_error_paired_reads_TEST.fasta"
+# reads = "test/project1a_10000_with_error_paired_reads_TEST.fasta"
 
-referenceGenome = sys.argv[1]
-reads = sys.argv[2]
+# referenceGenome = sys.argv[1]
+# reads = sys.argv[2]
 
 aligner = readAligner(genomeFile=referenceGenome, readFile=reads)
